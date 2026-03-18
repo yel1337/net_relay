985f77561aca3676b49e69b4e0335dd32e1f60e1
yel1337 <daniel.salvaloza2002@gmail.com>
2026-03-19 00:43:21 +0800

    updated host url

--- net_ssl/ssl_utils.py
+++ net_ssl/ssl_utils.py
@@ -5,7 +5,7 @@ import socket
 # fetching server's certificate
 class SSL_Utils:
     def get_cert():
-        hostname="www.chatkool.com"
+        hostname="www.google.com"
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         ctx.load_verify_locations("/etc/ssl/certs/ca-bundle.crt")
         conn = ctx.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
