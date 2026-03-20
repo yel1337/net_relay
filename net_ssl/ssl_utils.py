39fe565762f1bdc0129bad045c1b2bef9774c15d
yel1337 <daniel.salvaloza2002@gmail.com>
2026-03-20 10:09:49 +0800

    added _validate within class to validate server certificate againts root CA using ca-bundle.crt

--- net_ssl/ssl_utils.py
+++ net_ssl/ssl_utils.py
@@ -4,10 +4,21 @@ import socket
 # Just to start it off 
 # fetching server's certificate
 class SSL_Utils:
-    def get_cert(hostname, port):
+    def __init__(self):
+        self.ca_path = "/etc/ssl/certs/ca-bundle.crt"
+    def get_cert(self, hostname, port):
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        ctx.load_verify_locations("/etc/ssl/certs/ca-bundle.crt")
+        # ctx.load_verify_locations(self.ca_path)
         conn = ctx.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
         conn.connect((hostname, port))
         # Return site's certificate
         return f"{conn.getpeercert()}"
+
+    def _validate(self, hostname, port):
+        # Validate server certificate againts root CA stored in /etc/ssl/certs/ca-bundle.crt
+        try:
+            cert_val = ssl.get_server_certificate(hostname, port, ca_certs = self.ca_path)
+            if cert_val:
+                return 1 
+        except ssl.SSLVerificationError as e:
+            return f"{e}"
